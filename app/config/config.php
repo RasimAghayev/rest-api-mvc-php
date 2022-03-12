f19830cc4068e316037683a4f9c43bd76861bbf8
Rasim Aghayev <2940564+RasimAghayev@users.noreply.github.com>
2022-03-13 03:48:26 +0400

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,17 @@
 <?php
   // DB Params
   define("DB_HOST", "localhost");
-  define("DB_USER", "bscgroup_test");
-  define("DB_PASS", "!@#Rasim456");
-  define("DB_NAME", "bsc_rest");
+  define("DB_USER", "root");
+  define("DB_PASS", "");
+  define("DB_NAME", "rest-api-mvc-php");
   define('CHARSET', 'utf8');
   define('SESSION_TIME', 1800);
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://bsc-rest.rst/api');
+  define('URLROOT', 'http://rest-api-mvc-php/api');
   // Site Name
-  define('SITENAME', 'Test');
-  define('JWT_SECCRET_KEY', 'MisaR098*()');
+  define('SITENAME', 'rest-api-mvc-php');
+  define('JWT_SECCRET_KEY', 'rest-api-mvc-php123!@#!$!@546asda');
 
